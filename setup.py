2780de63de7cd8e4963bcc242474d15cbd4b1892
Erin Trochim <28066662+edtrochim@users.noreply.github.com>
2025-10-03 14:21:11 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="colab-plotting-theme",
     version="0.1.0",
     author="Erin Trochim",
     description="Custom plotting theme for Google Colab with Open Sans font",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/edtrochim/plotnine-plotting-theme",
     packages=find_packages(),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",  # Fixed to match LICENSE
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
